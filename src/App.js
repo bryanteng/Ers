7a23489cbca98beaec427bab6b3dd037836aeea7
Bryan Teng <bteng1226@gmail.com>
2021-03-16 17:31:06 -0400

    added navbar, homepage, and stylings for a faux login page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import './App.css';
+import React, { useState, useEffect, Fragment } from 'react'
+
+import Navbar from './components/navbar'
+import Homepage from './containers/homepage'
+
 import Rules from './containers/rules'
 import Game from './containers/game'
 import NewPlayerInput from './components/newPlayerInput'
 
 function App() {
+
+  const [user, setUser] = useState("")
+  const [loggedIn, setLoggedIn] = useState(false)
+
+  useEffect(function updateTitle() {
+    document.title = "Welcome " + user;
+  },[user]);
+
+  console.log(user, "dis user")
   return (
     <div className="App">
+    {loggedIn ?
+    <Fragment>
+      <Navbar user={user} />
       hi
       <Rules />
       <NewPlayerInput />
       <Game />
+    </Fragment>
+    :
+    <Homepage loggedIn={loggedIn} setLoggedIn={setLoggedIn} user={user} setUser={setUser} />
+  }
+
+
 
     </div>
   );
